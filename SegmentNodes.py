c323c6beebf28965583876f47d0847c3d8e5a105
Angel Rodriguez Junquera <angel@sublimestudio.ch>
2024-03-28 17:38:35 +0100

    Upadte

--- SegmentNodes.py
+++ SegmentNodes.py
@@ -68,7 +68,7 @@ class SegmentNode:
             boxes.append(torch.tensor(box))
 
             transform = transforms.ToTensor()
-            tensor_image = transform(image).permute(1, 2, 0)
+            tensor_image = transform(image)
             print(tensor_image.shape)
             images.append(tensor_image)
 
